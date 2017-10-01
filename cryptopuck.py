8a773bca7c53865f040999ffeb1a6c76de21dccb
Dimitris Platis <kurukii88@gmail.com>
2017-10-01 20:02:45 +0200

    Use normal notifier instead for simplicity

--- cryptopuck.py
+++ cryptopuck.py
@@ -1,11 +1,12 @@
-import os, getpass, asyncore, pyinotify, time
+import os, getpass, pyinotify, time
 import encrypt
 
 class EventHandler(pyinotify.ProcessEvent):
     def process_IN_CREATE(self, event):
         if (os.path.isdir(event.pathname)):
-            print ("Creating:" + event.pathname)
-            time.sleep(1)  # Wait for the volume to be fully mounted
+            print ("New mounted volume detected: " + event.pathname)
+            # Wait for the volume to be mounted and avoid permission errors
+            time.sleep(1)
             encrypt.run(event.pathname, event.pathname, "./key.public")
             print("Done with encryption of " + event.pathname)
 
@@ -16,10 +17,10 @@ def main():
     wm = pyinotify.WatchManager()  # Watch Manager
     mask = pyinotify.IN_CREATE  # watched events
 
-    notifier = pyinotify.AsyncNotifier(wm, EventHandler())
+    notifier = pyinotify.Notifier(wm, EventHandler())
     wdd = wm.add_watch(mountpoint, mask)
 
-    asyncore.loop()
+    notifier.loop()
 
 if __name__ == "__main__":
     main()
